bee8dfa05fc5426d14420cc6fba62db8f01bda64
burd5 <austinburdette5@gmail.com>
2023-02-22 17:56:01 -0700

    edited so cursor shows on next line

--- band_name_generator.py
+++ band_name_generator.py
@@ -6,8 +6,8 @@
 print('Welcome to the Band Name Generator!')
 
 # prompts user to answer two questions and stores answers in variables
-user_city = input("What's the name of the city you grew up in?")
-pet_name = input("What's your pet's name?")
+user_city = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
 
 # prints final answer which is a combination of the two responses and a space in between
 print(user_city + ' ' + pet_name)
