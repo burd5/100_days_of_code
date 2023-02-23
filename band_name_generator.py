89215d062bdeace3859702ab9c645afeaf76edae
burd5 <austinburdette5@gmail.com>
2023-02-22 17:54:26 -0700

    fixed typo

--- band_name_generator.py
+++ band_name_generator.py
@@ -7,7 +7,7 @@ print('Welcome to the Band Name Generator!')
 
 # prompts user to answer two questions and stores answers in variables
 user_city = input("What's the name of the city you grew up in?")
-pet_name = input("DenverWhat's your pet's name?")
+pet_name = input("What's your pet's name?")
 
 # prints final answer which is a combination of the two responses and a space in between
 print(user_city + ' ' + pet_name)
