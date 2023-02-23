4af0a5edab0d01db6dcb14d012d7e71ef30573b9
burd5 <austinburdette5@gmail.com>
2023-02-22 17:53:35 -0700

    completed the band generator problem

---
+++ band_name_generator.py
@@ -0,0 +1,19 @@
+# Band Name Generator
+# Includes title and two questions: What's the name of the city you grew up in? and What's your pet's name?
+# Print result of answers combined
+
+# prints title to page
+print('Welcome to the Band Name Generator!')
+
+# prompts user to answer two questions and stores answers in variables
+user_city = input("What's the name of the city you grew up in?")
+pet_name = input("DenverWhat's your pet's name?")
+
+# prints final answer which is a combination of the two responses and a space in between
+print(user_city + ' ' + pet_name)
+
+
+
+
+
+
