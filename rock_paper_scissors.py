001f52f3be54e78ccd781f85a3c8fcfe40260590
burd5 <austinburdette5@gmail.com>
2023-02-25 16:51:39 -0700

    added false answer response for game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -32,6 +32,9 @@ choices = [rock, paper, scissors]
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors\n"))
 computer_choice = random.randint(0,2)
 
+if user_choice != 0 or user_choice != 1 or user_choice != 2:
+    print('Not an answer option, you lose.')
+
 if user_choice == 0:
     if computer_choice == 0:
         print(f"You chose:\n{rock}\nComputer chose:\n{choices[computer_choice]}\nTie: Try Again")
